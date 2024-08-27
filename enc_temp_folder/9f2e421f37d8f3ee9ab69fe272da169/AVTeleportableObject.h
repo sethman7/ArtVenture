453a849492ff611269a041f367d7249df153a654
yisy0713 <yisy0713@naver.com>
2024-08-27 16:54:15 +0900

    도슨트BT 업데이트
    플레이어가 보던 작품에 해당하는 설명 제공

---
+++ enc_temp_folder/9f2e421f37d8f3ee9ab69fe272da169/AVTeleportableObject.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/AVInteractableInterface.h"
+#include "Interface/AVTeleportabletInterface.h"
+#include "AVTeleportableObject.generated.h"
+
+UCLASS()
+class ARTVENTURE_API AAVTeleportableObject : public AActor, public IAVInteractableInterface , public IAVTeleportabletInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAVTeleportableObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleportable")
+	FName WorldName;
+	
+	UFUNCTION()
+	virtual void Teleport() override;
+	virtual void Interact() override;
+};
