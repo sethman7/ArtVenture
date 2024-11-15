d531fa1272100b7213238010d6e0de8b1143bc23
sethman7 <131145377+sethman7@users.noreply.github.com>
2024-11-15 10:13:13 +0900

    Fix Bug and Convert C++ to BP

--- Source/ArtVenture/Prop/AVTeleportableObject.h
+++ Source/ArtVenture/Prop/AVTeleportableObject.h
@@ -1,39 +1,41 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "Interface/AVInteractableInterface.h"
-#include "AVTeleportableObject.generated.h"
-
-UCLASS(Abstract)
-class ARTVENTURE_API AAVTeleportableObject : public AActor, public IAVInteractableInterface 
-{
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AAVTeleportableObject();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh)
-	TObjectPtr<UStaticMeshComponent> Mesh;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleportable") // 블프로 WorldName값이 필요해서 수정햇서
-	FName DestinationWorldName;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleportable")
-	FString CurrentWorldName;
-
-
-
-	UFUNCTION(BlueprintCallable)
-	virtual void Interact() override;
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "GameFramework/Actor.h"
+//#include "Interface/AVInteractableInterface.h"
+//#include "AVTeleportableObject.generated.h"
+//
+//UCLASS(Abstract)
+//class ARTVENTURE_API AAVTeleportableObject : public AActor, public IAVInteractableInterface 
+//{
+//	GENERATED_BODY()
+//	
+//public:	
+//	// Sets default values for this actor's properties
+//	AAVTeleportableObject();
+//
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh)
+//	TObjectPtr<UStaticMeshComponent> Mesh;
+//
+//	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleportable") // 블프로 WorldName값이 필요해서 수정햇서
+//	//FName DestinationWorldName;
+//	
+//	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Teleportable")
+//	FName WorldName;
+//	FString CurrentWorldName;
+//
+//
+//	UFUNCTION(BlueprintCallable)
+//	virtual void Interact() override;
+//
+//	void SpawnPlayer();
+//};
